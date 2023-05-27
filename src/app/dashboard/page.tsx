e7ca497997807b778cabf7fbb1cf05bca6b60ee3
thientantran <thientantm@gmail.com>
2023-05-27 09:14:12 +0700

    npx create-next-app@latest --typescript

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div>Dashboard
+      <h1>DASH BOARD TAN TON TEN</h1>
+    </div>
+    
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import styles from './page.module.css'
+import Dashboard from './dashboard/page'
+import Head from 'next/head'
+import Header from './components/Header/page'
+import SideMenu from './components/SideMenu/page'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Data Dashboard</title>
+        <meta name="description" content='Data dashboard'/>
+        <meta name='viewpoint' content='width-device-width, initial-scale=1'/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+<main className={styles.main}>
+      <Header/>
+      <SideMenu/>
+      <Dashboard/>
+    </main>
+    </div>
+    
+  )
+}
